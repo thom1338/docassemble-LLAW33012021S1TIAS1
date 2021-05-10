4d22eed22276098e4efa9ac857d11303b6a226e7
Spencer Thomas <79789779+thom1338@users.noreply.github.com>
2021-05-10 19:49:45 +0930

    Thom1338

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012021S1TIAS1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012021S1TIAS1\n\nA docassemble extension.\n\n## Author\n\nMark Ferraretto, mark.ferraretto@flinders.edu.au\n\n',
+      long_description='# docassemble.LLAW33012021S1TIAS1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMark Ferraretto, mark.ferraretto@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Mark Ferraretto',
       author_email='mark.ferraretto@flinders.edu.au',
